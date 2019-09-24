80940ae75b82c27e33ba93f07619eb5bfb3fdfbb
Thibault <thibault.de-rubercy@student.ecp.fr>
2019-09-24 16:27:17 +0200

    Add exchanges with customer

--- serveur.py
+++ serveur.py
@@ -1,7 +1,7 @@
 import socket
 
 hote = ''
-port = 5000
+port = 5001
 
 connexion_principale = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 connexion_principale.bind((hote,port))
@@ -11,3 +11,13 @@ print("Le serveur est prêt sur le port numéro {}".format(port))
 connexion_avec_client, infos_connexion = connexion_principale.accept()
 connexion_avec_client.send(b"Welcome on the whiteboard, you're connected on the server")
 serveur_lance = True
+
+while serveur_lance:
+    msg_recu = connexion_avec_client.recv(1024)
+    msg_recu = msg_recu.decode()
+    if msg_recu == "END":
+        serveur_lance = False
+        connexion_avec_client.send(b"end")
+    else:
+        connexion_avec_client.send(b"ok")
+
