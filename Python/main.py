d23e4e25174e31f616f325a92d6b2b9914ae005c
Thibault <thibault.de-rubercy@student.ecp.fr>
2019-09-24 15:45:54 +0200

    Create server connexion

--- Python/main.py
+++ Python/main.py
@@ -1,1 +1,10 @@
-print('hello world')
+import pygame
+import socket
+
+hote = ''
+port = 5000
+
+connexion_principale = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+connexion_principale.bind((hote,port))
+connexion_principale.listen()
+print("Le serveur est prêt sur le port {}".format(port))
