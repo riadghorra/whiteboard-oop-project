1debc8722bd6f95e1c1eec684b0144ce1bfba517
Thibault <thibault.de-rubercy@student.ecp.fr>
2019-09-24 16:10:02 +0200

    Add a comment

--- serveur.py
+++ serveur.py
@@ -6,7 +6,7 @@ port = 5000
 connexion_principale = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 connexion_principale.bind((hote,port))
 connexion_principale.listen()
-print("Le serveur est prêt sur le port {}".format(port))
+print("Le serveur est prêt sur le port numéro {}".format(port))
 
 connexion_avec_client, infos_connexion = connexion_principale.accept()
 connexion_avec_client.send(b"Welcome on the whiteboard, you're connected on the server")
