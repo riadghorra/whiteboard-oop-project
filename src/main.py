0236ff774249c5324208e4a26700f8c4579d80c5
Arthur Lindoulsi <arthur@MacBook-Pro-de-Arthur-2.local>
2019-09-25 15:06:43 +0200

    more classes

---
+++ src/main.py
@@ -0,0 +1,10 @@
+from white_board import white_board
+
+
+def main():
+    board = white_board()
+    board.start()
+
+
+if __name__ == '__main__':
+    main()
--- src/white_board.py
+++ src/white_board.py
@@ -7,21 +7,22 @@ with open('config.json') as json_file:
     config = json.load(json_file)
 
 
-def main():
-    pygame.init()
-    screen = pygame.display.set_mode([config["width"], config["length"]])
-    screen.fill(config["board_background_color"])
-    done = False
-    while done == False:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                done = True
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                to_draw = point()
-                to_draw.draw(screen, event.dict['pos'], event.dict['button'], config["point_color"],
-                             config["point_radius"])
-    pygame.quit()
+class white_board():
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode([config["width"], config["length"]])
+        self.screen.fill(config["board_background_color"])
+        self.done = False
+
+    def start(self):
+        while self.done == False:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.done = True
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    to_draw = point()
+                    to_draw.draw(self.screen, event.dict['pos'], event.dict['button'], config["point_color"],
+                                 config["point_radius"])
+        pygame.quit()
 
 
-if __name__ == '__main__':
-    main()
